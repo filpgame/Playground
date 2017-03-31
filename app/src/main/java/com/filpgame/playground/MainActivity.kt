d24451f5084feb98eb11c83f478ba82dc6717e2a
Felipe Rodrigues <frodrigues@stone.com.br>
2017-03-31 01:11:22 -0300

    some simplified

--- app/src/main/java/com/filpgame/playground/MainActivity.kt
+++ app/src/main/java/com/filpgame/playground/MainActivity.kt
@@ -3,32 +3,29 @@ package com.filpgame.playground
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.navigation_home -> {
                 message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
                 message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
                 message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
             }
         }
-        false
+        return true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.setOnNavigationItemSelectedListener(this)
     }
-
 }
